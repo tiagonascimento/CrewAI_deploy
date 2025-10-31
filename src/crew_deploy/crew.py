33a4e07e3b1d367b097a3b530e0a75882196d0af
Tiago Rocha <tiago.rocha.nascimento@gmail.com>
2025-10-31 13:30:12 -0300

    ajustes

--- src/crew_deploy/crew.py
+++ src/crew_deploy/crew.py
@@ -15,9 +15,9 @@ import os
 class CrewDeploy():
     """CrewDeploy crew"""
 
-    llm = LLM(       
+    llm = LLM(        
         model="llama3-8b-8192",
-        api_key=os.environ["GROQ_API_KEY"],
+        api_key=os.getenv("GROQ_API_KEY"),
         base_url="https://api.groq.com/openai/v1",
         temperature=0.7
     )
